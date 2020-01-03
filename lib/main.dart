80695ff3742446b5d5aaaa36d111707c8ecfe987
kaleido01 <mbdkaleido@gmail.com>
2020-01-03 23:07:43 +0900

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import "./Question.dart";
+import "./Answer.dart";
 
 void main() => runApp(Myapp());
 
-addNumbers(num1, num2) {
-  print(num1 + num2);
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
 }
 
-class Myapp extends StatelessWidget {
+class _MyappState extends State<Myapp> {
+  int _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      {
+        "questionTitle": "What\'s is your favorite color",
+        "answer": {"a", "b", "c"}
+      },
+      {
+        "questionTitle": "What\'s is your favorite animal",
+        "answer": {"a", "b", "c", "d"}
+      },
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("this is title"),
-          backgroundColor: Color.fromRGBO(222, 0, 70, 1),
-        ),
-        body: Text("this is body"),
-      ),
+          appBar: AppBar(
+            title: Text("this is title"),
+            backgroundColor: Color.fromRGBO(222, 0, 70, 1),
+          ),
+          body: Column(
+            children: [
+              Question(questions[_questionIndex]["questionText"]),
+              ...questions.map((question) {
+                return Answer(question);
+              })
+            ],
+          )),
     );
   }
 }
